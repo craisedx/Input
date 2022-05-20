a14e68e2150568f21cb1094cb776b07d13cde4a8
Дмитрий Медведев <greatsmith184@gmail.com>
2022-05-20 19:57:12 +0300

    Added Auth and added KendoLib.

---
+++ Input/Controllers/AccountController.cs
@@ -0,0 +1,143 @@
+﻿using System.Threading.Tasks;
+using Input.Constants;
+using Input.Constants.InfoMessages;
+using Input.Email;
+using Input.Migrations;
+using Input.Models;
+using Input.ViewModels.Account;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Input.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationContext db;
+        private readonly SignInManager<User> signInManager;
+        private readonly UserManager<User> userManager;
+        
+        public AccountController(
+            UserManager<User> userManager,
+            SignInManager<User> signInManager,
+            ApplicationContext context)
+        {
+            db = context;
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+        
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+        
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            return View(new LoginViewModel {ReturnUrl = returnUrl});
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = userManager.FindByNameAsync(model.UserName).Result;
+                var signInResult = await signInManager
+                    .PasswordSignInAsync(model.UserName, model.Password, true, false);
+                if (signInResult.Succeeded)
+                {
+                    if (user.EmailConfirmed)
+                    {
+                        if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                            return Redirect(model.ReturnUrl);
+
+                        await db.SaveChangesAsync();
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                    ModelState.AddModelError(string.Empty, UserErrorsConstants.EmailNotConfirmed);
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, UserErrorsConstants.LoginPasswordEnteredIncorrectly);
+                }
+            }
+
+            return View(model);
+        }
+
+        [Route("Confirm")]
+        public IActionResult Confirm()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> ConfirmEmail(string userId, string code)
+        {
+            if (userId == null || code == null)
+            {
+                return View("Error");
+            }
+
+            var user = await userManager.FindByIdAsync(userId);
+            
+            if (user == null)
+            {
+                return View("Error");
+            }
+
+            var result = await userManager.ConfirmEmailAsync(user, code);
+
+            if (!result.Succeeded) return View("Error");
+            
+            return RedirectToAction("Index", "Home");
+        }
+        
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User
+                {
+                    Email = model.Email, UserName = model.UserName,
+                    UserPhoto = UserInfoConstants.BaseUserPhoto
+                };
+
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    var code = await userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var callbackUrl = Url.Action(
+                        "ConfirmEmail",
+                        "Account",
+                        new {userId = user.Id, code = code},
+                        protocol: HttpContext.Request.Scheme);
+
+                    await EmailWork.SendEmailDefault(model.Email, UserInfoConstants.AccountConfirmation,
+                        UserInfoConstants.SendConfirmEmail(callbackUrl));
+                    
+                    return RedirectToAction("Confirm", "Account");
+                }
+
+                foreach (var error in result.Errors) ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Input/Email/EmailWork.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Input.Constants.InfoMessages;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace Input.Email
+{
+    public static class EmailWork
+    {
+        public static async Task SendEmailDefault(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+            
+            emailMessage.From.Add(new MailboxAddress(UserInfoConstants.EmailName, UserInfoConstants.EmailService));
+            emailMessage.To.Add(new MailboxAddress(string.Empty, email));
+            
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync(UserInfoConstants.ConnectHost, 587);
+                await client.AuthenticateAsync(UserInfoConstants.EmailServiceGmail, UserInfoConstants.PasswordService);
+                await client.SendAsync(emailMessage);
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
